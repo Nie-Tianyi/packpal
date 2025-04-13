e36b70b91d6ce89e5e076f5c3f271599dc019816
nty-pc <1355705558@qq.com>
2025-04-13 20:20:34 +0800

    refactor: refactor project structure

--- src/functions/build.rs
+++ src/functions/build.rs
@@ -1,21 +1,21 @@
-use pulldown_cmark::{Options, html};
+use pulldown_cmark::{Options, html, Parser};
 use std::fs;
 
 pub fn build() {
     unimplemented!()
 }
 
-// fn hydrate_post(md_path: String) {
-//     let markdown = fs::read_to_string(POSTS_FOLDER.to_owned() + "比特币.md")?;
-//     let parser = MarkdownParser::new_ext(&*markdown, Options::all());
-//
-//     let mut content = String::new();
-//     html::push_html(&mut content, parser);
-//
-//     let template = fs::read_to_string(TEMPLATE_FOLDER.to_owned() + "posts_template.html")?;
-//     let res = template
-//         .replace("<PostHeading/>", &*format!("<h1>{}</h1>", "比特币"))
-//         .replace("<ContentRoot/>", &*content);
-//
-//     fs::write(BUILD_FOLDER.to_owned() + "比特币.html", res)?;
-// }
+fn hydrate_post(md_path: String) {
+    let markdown = fs::read_to_string(md_path).unwrap();
+    let parser = Parser::new_ext(&*markdown, Options::all());
+
+    let mut content = String::new();
+    html::push_html(&mut content, parser);
+
+    let template = fs::read_to_string("./templates/".to_owned() + "posts_template.html")?;
+    let res = template
+        .replace("<PostHeading/>", &*format!("<h1>{}</h1>", "比特币"))
+        .replace("<ContentRoot/>", &*content);
+
+    fs::write("BUILD_FOLDER".to_owned() + "比特币.html", res)?;
+}
